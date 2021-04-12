47cb0ff5f867ed8c08b5ba298270682fb506e87e
abdul-malik <abdulmalikmohamed360@gmail.com>
2021-04-12 16:27:19 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Task 1 Sample Solution
+print("Hello World!")
+
+# Task 2 Sample Solution
+_PI = 3.14
+radius = int(input("Input the radius of the circle : "))
+area = _PI * radius**2
+print("the area of a circle is: " + str(area))
+
+# Task 3 Sample Solution
+diameter= 2 * radius
+print("The diameter of a circle is: " + str(diameter))
